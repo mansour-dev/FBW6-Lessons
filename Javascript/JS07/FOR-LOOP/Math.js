533a40f7cea295e4f0560d6ea3e5ce716df62c70
Mansour-Tumeh <mansou.tumeh@digitalcareerinstitute.org>
2020-05-06 14:03:41 +0200

    Math Object in js

---
+++ Javascript/JS07/FOR-LOOP/Math.js
@@ -0,0 +1,79 @@
+const num1 = 50;
+const num2 = 100;
+let val;
+// Simple Math with Numbers
+
+val = num1 + num2;
+val = num1 * num2;
+val = num2 - num1;
+val = num2 / num1;
+val = num2 % num1;
+
+// Math object 
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.6);
+val = Math.ceil(2.4);
+val = Math.floor(2.9);
+val = Math.sqrt(64);
+val = Math.abs(-2);
+val = Math.pow(8,2) // => 8 * 8   
+val = Math.min(3,-7,33,0,6,10);
+val = Math.max(2,55,100,7);
+val = Math.random();
+val = Math.random() * 20 + 1 ;
+val = Math.floor(Math.random() * 20  +1 )
+
+
+console.log(val);
+
+// function randomDString(length){
+//     let result = '';
+//     let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+//     const charactersLength = characters.length;
+//     for(let i = 0 ; i< length;i++ ){
+//         console.log('the random number is :' , Math.floor(Math.random() * charactersLength));
+//         console.log(characters.charAt(Math.floor(Math.random() * charactersLength)));
+        
+//         result += characters.charAt(Math.floor(Math.random() * charactersLength));
+
+//     }
+
+//     return result;
+// }
+
+// console.log(randomDString(5))
+
+
+
+
+
+//val = Math.floor(Math.random() * 20  +1 )
+
+
+// create a function to return random number between 1 and 30 
+
+function getRandom(){
+    return Math.floor(Math.random() * 30 +1);
+} 
+
+// console.log('the random number is : ',getRandom());
+
+///////////////////////////////////////////////////////
+
+// create function to return random color from this array 
+
+const colors = ['red','gray','black','yellow','green','pink','magenta','orange'];
+
+function randomColor(arr){
+    const length = arr.length; // getting the length of the array
+
+    let i =   Math.floor( Math.random() * length ) // generating the random number between 0 and the length of the array
+
+    return arr[i]; // getting the element form the array using the random index
+
+}
+// console.log('my random color is : ' , randomColor(colors))
+document.body.style.background=randomColor(colors);
+
+
---
+++ Javascript/JS07/FOR-LOOP/app.js
@@ -0,0 +1,78 @@
+// const colors = ['red', 'yellow','green','magenta','black','lightgreen'];
+// for (let i=0; i<colors.length; i+=1){
+// if (colors[i] === 'green'){
+//       console.log('the index of green is ',i)
+//     break;
+// }
+// console.log(colors[i]);
+// document.write(colors[i]+ '<br>');
+// }
+// let message = "hello";
+// message = 1234565465
+// console.log(typeof message);
+// let car = "BMW" ;
+
+// console.log(typeof car);
+
+// if(typeof x ==='undefined'){
+// //     console.log('x is undefined');
+    
+// // }
+
+// const hobbies = ['movies', 'music'];
+// const address = {
+//     city: 'Hamburg',
+//     street: 'wandalenweg 30'
+//   }
+//   const today = new Date();
+// console.log(typeof today);
+
+
+
+let val;
+
+// Number to String
+val = String(555);
+val = String(4 + 4 );
+
+// Boolean to String
+val = String(true);
+
+// Date to String
+val = String(new Date());
+
+// Array to String
+val = String([2,4,5,6])
+
+// tostring()
+
+val = (5).toString();
+val = (true).toString();
+
+// String to Number
+val = Number('5');
+val = Number(false);
+val = Number(null);
+val = Number('hallo');
+val = Number([1,23,6]);
+
+val = parseInt('100.66'); // to remove the . and to convert to a number
+ val = parseFloat('100.224654654564654') // to keep the . and to convert to a number
+
+
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+console.log(val.toFixed(2));
+
+const val1 = String(5);
+const val2 = 6;
+const sum = Number(val1)+val2;
+
+console.log(sum);
+console.log(typeof sum);
+
+
+
+
+
