9d1be93b7655fcba28afc197aefdb153e139c184
Mansour-Tumeh <mansou.tumeh@digitalcareerinstitute.org>
2020-05-27 13:34:09 +0200

    assignment in classes

---
+++ Javascript/JS15/assignment.js
@@ -0,0 +1,12 @@
+class Employee {
+	constructor (firstname, lastname) {
+		this.firstname = firstname;
+        this.lastname = lastname;
+        
+		// Complete the code!
+	}
+}
+
+//Create the  properties fullname and email in the Employee class. Given a person's first and last names:
+// Form the fullname by simply joining the first and last name together, separated by a space.
+// Form the email by joining the first and last name together with a . in between, and follow it with @company.com at the end. Make sure everything is in lowercase.
