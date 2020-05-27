68ec2af32eae57a2235dd1f89b24a3710a8ca4bc
Mansour-Tumeh <mansou.tumeh@digitalcareerinstitute.org>
2020-05-27 14:20:04 +0200

    assignment in classes update

--- Javascript/JS15/assignment.js
+++ Javascript/JS15/assignment.js
@@ -1,7 +1,8 @@
 class Employee {
-	constructor (firstname, lastname) {
+	constructor (firstname, firstname) {
 		this.firstname = firstname;
-        this.lastname = lastname;
+        this.lastname = firstname;
+        this.fullName = this.firstname + this.lastname;
         
 		// Complete the code!
 	}
@@ -10,3 +11,45 @@ class Employee {
 //Create the  properties fullname and email in the Employee class. Given a person's first and last names:
 // Form the fullname by simply joining the first and last name together, separated by a space.
 // Form the email by joining the first and last name together with a . in between, and follow it with @company.com at the end. Make sure everything is in lowercase.
+
+
+let developer = new Employee('John','Doe');
+console.log(developer.fullName);
+console.log(developer.email);
+
+
+////////////////////////////////////////////////////////
+ // Write functions to add(), subtract(), multiply() and divide()
+    /* 
+    Create functions for the Calculator class that can do the following:
+    Add two numbers.
+    Subtract two numbers.
+    Multiply two numbers.
+    Divide two numbers.
+    The functions should return the result of the calculation.
+        
+    */
+class Calculator {
+    constructor(){
+
+    }
+   
+   add(firstNumber,lastNumber){
+       return firstNumber + lastNumber ;
+
+   }
+   subtract(){
+
+   }
+   multiply(){
+
+   }
+   divide(){
+
+   }
+}
+let calculator = new Calculator();
+calculator.add(10, 5) // 15
+calculator.subtract(10, 5) // 5
+calculator.multiply(10, 5) // 50
+calculator.divide(10, 5) // 2
