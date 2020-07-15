8eef63ca590d8da5a711f719dfbdac28ccd58124
mansour-tumeh <mansour-tumeh@digitalcareerinstitute.org>
2020-07-15 11:09:04 +0300

    Regular Expression Exercise

--- Javascript/JS35/Regular Expressions/example/app.js
+++ Javascript/JS35/Regular Expressions/example/app.js
@@ -4,8 +4,8 @@ const email = document.getElementById('email');
 const phone = document.getElementById('phone');
 
 name.addEventListener('blur' ,validateName);
-// email.addEventListener('blur' ,validateEmail);
-// zip.addEventListener('blur' ,validateZip);
+ email.addEventListener('blur' ,validateEmail);
+ zip.addEventListener('blur' ,validateZip);
 // phone.addEventListener('blur' ,validatePhone);
 
 function validateName(e){
@@ -23,5 +23,34 @@ function validateName(e){
     
   }
 
+  
+}
+function validateZip(){
+  const reg = /^[0-9]{5}$/;
+  if(!reg.test(zip.value)){
+    zip.classList.add('is-invalid');
+    zip.classList.remove('is-valid');
+  }
+  else{
+    // all is fine 
+  zip.classList.remove('is-invalid');
+  zip.classList.add('is-valid')
+  
+}
+
+}
+
+function validateEmail(){
+  const reg = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+  if(!reg.test(email.value)){
+    email.classList.add('is-invalid');
+    email.classList.remove('is-valid');
+
+  }
+  else{
+    email.classList.remove('is-invalid');
+    email.classList.add('is-valid')
+
+  }
   
 }
